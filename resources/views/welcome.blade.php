362f11ba4dc134cbe768f51319a76c6d34bac32b
Evid Wong <76572677@qq.com>
2018-05-03 17:02:44 +0000

    edit welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-Evid Wong
+                    Laravel - By Evid Wong
                 </div>
 
                 <div class="links">
